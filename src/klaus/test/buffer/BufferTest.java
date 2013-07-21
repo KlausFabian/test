dfe4d46b54fc008f0eacb1f818017ab3d75a3d33
KlausFabian <Klaus.Fabian@gmail.com>
2013-07-21 12:00:31 -0700

    Test of git upload

---
+++ src/klaus/test/buffer/BufferTest.java
@@ -0,0 +1,13 @@
+package klaus.test.buffer;
+
+public class BufferTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Git");
+	}
+
+}
